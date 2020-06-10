419734cd2f607f64fb6008c320806793b1ece547
Onno Valkering <onnovalkering@users.noreply.github.com>
2020-06-10 06:06:51 +0000

    test(exec): test new Docker execution API

--- brane-exec/tests/docker.rs
+++ brane-exec/tests/docker.rs
@@ -7,17 +7,21 @@ use tokio;
 async fn name() {
     let image = String::from("arithmetic:1.0.0");
     let image_file = PathBuf::from("./resources/arithmetic.tar");
-    let payload = json!({
-        "identifier": "1+1",
+    let mounts = None;
+    let working_dir = None;
+    let command = vec![String::from("exec"), base64::encode(serde_json::to_string(&json!({
+        "identifier": "test",
         "action": "add",
         "arguments": {
             "a": 1,
             "b": 1,
         },
-    });
+    })).unwrap())];
 
-    let exec_info = ExecuteInfo::new(image, Some(image_file), payload);
+    let exec_info = ExecuteInfo::new(image, Some(image_file), mounts, working_dir, Some(command));
     let (stdout, _) = run_and_wait(exec_info).await.unwrap();
+
+    println!("{}", stdout);
 
     let output: JValue = serde_json::from_str(&stdout).unwrap();
     assert_eq!(output["c"].as_i64().unwrap(), 2);
