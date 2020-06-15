ad9e6e4bd363015184e08c9703a8c4cdc2233671
Onno Valkering <onnovalkering@users.noreply.github.com>
2020-06-15 06:31:14 +0000

    feat(exec): add high-level CWL execution

--- brane-exec/src/delegate.rs
+++ brane-exec/src/delegate.rs
@@ -5,8 +5,10 @@ use specifications::common::Value;
 use specifications::instructions::ActInstruction;
 use openapiv3::OpenAPI;
 use std::io::BufReader;
-use serde_json::json;
-use std::fs::File;
+use serde_json::{json, Value as JValue};
+use std::fs::{self, File};
+use std::io::prelude::*;
+use std::io::Write;
 use std::path::PathBuf;
 
 type Map<T> = std::collections::HashMap<String, T>;
@@ -14,11 +16,81 @@ type Map<T> = std::collections::HashMap<String, T>;
 ///
 ///
 ///
-pub fn exec_cwl(
-    _act: &ActInstruction,
-    _arguments: Map<Value>,
+pub async fn exec_cwl(
+    act: &ActInstruction,
+    arguments: Map<Value>,
 ) -> Result<Option<Value>> {
-    unimplemented!();
+    // Create (temporary) working directory
+    let working_dir = tempfile::tempdir()?.into_path();
+    let working_dir_str = working_dir.to_string_lossy().to_string();
+
+    // Copy CWL document to working directory
+    if let Some(cwl_file) = act.meta.get("cwl_file").map(PathBuf::from) {
+        fs::copy(cwl_file, working_dir.join("document.cwl"))?;
+    } else {
+        // Try to get it from the registry
+        debug!("Not implemented!");
+        unimplemented!();
+    }
+
+    // Create file containing input arguments
+    let mut input = Map::<JValue>::new();
+    for (name, value) in arguments.iter() {
+        input.insert(name.clone(), value.as_json());
+    }
+
+    let input_path = working_dir.join("input.json");
+    let mut input_file = File::create(input_path)?;
+    writeln!(input_file, "{}", &serde_json::to_string(&input)?)?;
+
+    // Setup execution
+    let image = String::from("commonworkflowlanguage/cwltool");
+    let mounts = vec![
+        String::from("/var/run/docker.sock:/var/run/docker.sock"),
+        String::from("/tmp:/tmp"),
+        format!("{}:{}", working_dir_str, working_dir_str),
+    ];
+
+    let command = vec!["--quiet", "document.cwl", "input.json"].iter().map(|s| s.to_string()).collect();
+    let exec = ExecuteInfo::new(image, None, Some(mounts), Some(working_dir_str), Some(command));
+
+    let (stdout, stderr) = docker::run_and_wait(exec).await?;
+    if stderr.len() > 0 {
+        warn!("{}", stderr);
+    }
+
+    // Determine data type
+    let data_type = if let Some(data_type) = &act.data_type {
+        if data_type == "unit" {
+            return Ok(None);
+        }
+
+        data_type
+    } else {
+        return Ok(None);
+    };
+
+    let output: Map<JValue> = serde_json::from_str(&stdout)?;
+    for (_, value) in output.iter() {
+        let mut value_file = File::open(value["path"].as_str().unwrap())?;
+        let mut value = String::new();
+        value_file.read_to_string(&mut value)?;
+
+        let value = match data_type.as_str() {
+            "boolean" => Value::Boolean(value.parse()?),
+            "integer" => Value::Integer(value.parse()?),
+            "real" => Value::Real(value.parse()?),
+            "string" => Value::Unicode(value),
+            _ => {
+                debug!("Data type: {}", data_type);
+                unimplemented!()
+            }
+        };
+
+        return Ok(Some(value));
+    }
+
+    unreachable!();
 }
 
 ///
@@ -86,5 +158,23 @@ pub async fn exec_oas(
             }
         },
         _ => Ok(Some(output))
+    }
+}
+
+///
+///
+///
+pub fn exec_std(
+    act: &ActInstruction,
+    arguments: Map<Value>,
+) -> Result<Option<Value>> {
+    let package = act.meta.get("name").expect("No `name` property in metadata.");
+    let function = brane_std::FUNCTIONS.get(package).unwrap().get(&act.name).unwrap();
+
+    let output = function(&arguments)?;
+    if let Value::Unit = output {
+        Ok(None)
+    } else {
+        Ok(Some(output))
     }
 }
