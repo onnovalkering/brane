bc1d4c13be7ed2305363d52eb4f4f4aba17cd0ab
Onno Valkering <onnovalkering@users.noreply.github.com>
2020-09-20 15:59:22 +0000

    chore(example): update wordcount example

--- examples/wordcount/base64/run.py
+++ examples/wordcount/base64/run.py
@@ -4,17 +4,22 @@ import os
 import sys
 import yaml
 
-command = sys.argv[1]
-argument = os.environ['INPUT']
+def decode(s: str) -> str:
+  s = s.replace("\n", "")
+  b = base64.b64decode(s)
+  return b.decode("UTF-8")
 
-
-functions = {
-    "encode": lambda x: base64.b64encode(x.encode("UTF-8")).decode("UTF-8"),
-    "decode": lambda x: base64.b64decode(x).decode("UTF-8"),
-}
+def encode(s: str) -> str:
+  b = s.encode("UTF-8")
+  b = base64.b64encode(b)
+  return b.decode("UTF-8")
 
 if __name__ == "__main__":
-    argument = argument.replace("\n", "")
-    output = functions[command](argument)
-
-    print(yaml.dump({"output": output}))
+  command = sys.argv[1]
+  argument = os.environ["INPUT"]
+  functions = {
+    "decode": decode,
+    "encode": encode,
+  }
+  output = functions[command](argument)
+  print(yaml.dump({"output": output}))
