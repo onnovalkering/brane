bc0f33b61347fb129da5f5c94e8e0dce820cdd32
Onno Valkering <onnovalkering@users.noreply.github.com>
2020-11-18 11:04:16 +0000

    fix(examples): ensure an empty results directory in Camelyon package

--- examples/camelyon/install.sh
+++ examples/camelyon/install.sh
@@ -8,3 +8,11 @@ cd "PROCESS_L3"
 
 # Because 'n_samples' is twice in the config file, we prefix the first one (removed later).
 sed -ie '0,/n_samples/ s|n_samples|i_n_samples|' "doc/config.cfg"
+
+# Ensure an empty results directory
+if [ -d "$DIRECTORY" ]; then
+    rm -rf "results/*"
+else
+    mkdir "results"
+fi
+
