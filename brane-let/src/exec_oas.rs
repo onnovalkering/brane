e1f1efd9b9e3aceba7cab8d0256b5b6b70ab9da6
Onno Valkering <onnovalkering@users.noreply.github.com>
2021-05-10 09:57:33 +0000

    refactor(let): use brane-oas module for oas support

--- brane-let/src/exec_oas.rs
+++ brane-let/src/exec_oas.rs
@@ -1,10 +1,7 @@
 use crate::callback::Callback;
 use anyhow::{Context, Result};
-use openapiv3::{OpenAPI, Operation, Parameter as OParameter, ReferenceOr};
 use specifications::common::{Parameter, Type, Value};
 use specifications::package::PackageInfo;
-use std::fs::File;
-use std::io::BufReader;
 use std::path::PathBuf;
 
 type Map<T> = std::collections::HashMap<String, T>;
@@ -41,8 +38,11 @@ pub async fn handle(
         callback.started(None).await?;
     }
 
+    let oas_file = working_dir.join("document.yml");
+    let oas_document = brane_oas::parse_oas_file(oas_file)?;
+
     // Output variables are captured from the stdout
-    let stdout = execute(&function, &arguments, &working_dir).await?;
+    let stdout = brane_oas::execute(&function, &arguments, &oas_document).await?;
     let output = capture_output(&stdout, &function_info.return_type, &package_info.types)?;
 
     if let Some(value) = output {
@@ -96,112 +96,6 @@ fn initialize(
 ///
 ///
 ///
-pub async fn execute(
-    operation_id: &String,
-    arguments: &Map<Value>,
-    working_dir: &PathBuf,
-) -> Result<String> {
-    let oas_file = working_dir.join("document.yml");
-    let oas_reader = BufReader::new(File::open(&oas_file)?);
-    let oas_document: OpenAPI = serde_yaml::from_reader(oas_reader)?;
-
-    let base_url = &oas_document
-        .servers
-        .first()
-        .expect("OAS document requires a server.")
-        .url;
-
-    let (path, method, operation) = get_operation(operation_id, &oas_document)?;
-    let mut operation_url = format!("{}{}", base_url, path);
-
-    for parameter in &operation.parameters {
-        if let ReferenceOr::Item(parameter) = parameter {
-            match parameter {
-                OParameter::Path { parameter_data, .. } => {
-                    let name = &parameter_data.name;
-                    let value = arguments.get(name).expect("Missing argument.");
-                    match value {
-                        Value::Unicode(value) => {
-                            operation_url = operation_url.replace(&format!("{{{}}}", name), &value);
-                        }
-                        _ => unimplemented!(),
-                    }
-                }
-                _ => unimplemented!(),
-            }
-        } else {
-            unreachable!();
-        }
-    }
-
-    let client = reqwest::Client::builder().user_agent("HTTPie/1.0.3").build()?;
-
-    // Perform the requirest
-    let client = match method.as_str() {
-        "delete" => client.delete(&operation_url),
-        "get" => client.get(&operation_url),
-        "patch" => client.patch(&operation_url),
-        "post" => client.post(&operation_url),
-        "put" => client.put(&operation_url),
-        _ => unreachable!()
-    };
-
-    let response = client.send().await?.text().await?;
-    Ok(response)
-}
-
-///
-///
-///
-pub fn get_operation(
-    operation_id: &String,
-    oas_document: &OpenAPI,
-) -> Result<(String, String, Operation)> {
-    let (path, method, operation) = oas_document
-        .paths
-        .iter()
-        .find_map(|(path, path_item)| {
-            if let ReferenceOr::Item(path_item) = path_item {
-                // Check each method-operation to see if the operation ID matches.
-                let check = |op: &Option<Operation>| {
-                    if let Some(op) = op {
-                        if let Some(id) = &op.operation_id {
-                            if operation_id == id.to_lowercase().as_str() {
-                                return true;
-                            }
-                        }
-                    }
-
-                    false
-                };
-
-                if check(&path_item.delete) {
-                    return Some((path, "delete", path_item.delete.clone()));
-                }
-                if check(&path_item.get) {
-                    return Some((path, "get", path_item.get.clone()));
-                }
-                if check(&path_item.patch) {
-                    return Some((path, "patch", path_item.patch.clone()));
-                }
-                if check(&path_item.post) {
-                    return Some((path, "post", path_item.post.clone()));
-                }
-                if check(&path_item.put) {
-                    return Some((path, "put", path_item.put.clone()));
-                }
-            }
-
-            None
-        })
-        .expect("Mismatch in operation id");
-
-    Ok((path.clone(), method.to_string(), operation.unwrap()))
-}
-
-///
-///
-///
 fn capture_output(
     stdout: &String,
     return_type: &String,
@@ -210,7 +104,38 @@ fn capture_output(
     let json = serde_json::from_str(stdout)?;
     let output = Value::from_json(&json);
 
+    let filter = |object: &Value, c_type: &Type| {
+        let mut filtered = Map::<Value>::new();
+
+        if let Value::Struct { properties, .. } = object {
+            for p in &c_type.properties {
+                let property = properties
+                    .get(&p.name)
+                    .with_context(|| format!("Cannot find {} in output (required)", p.name))
+                    .unwrap();
+
+                filtered.insert(p.name.to_string(), property.clone());
+            }
+
+            return Value::Struct { data_type: c_type.name.clone(), properties: filtered };
+        }
+
+        object.clone()
+    };
+
     match &output {
+        Value::Array { entries, .. } => {
+            if let Some(c_types) = c_types {
+                let c_type = return_type.strip_suffix("[]").unwrap();
+
+                if let Some(c_type) = c_types.get(c_type) {
+                    let entries = entries.iter().map(|e| filter(e, c_type)).collect();
+                    return Ok(Some(Value::Array { entries, data_type: return_type.clone() }));
+                }
+            }
+
+            return Ok(Some(output));
+        }
         Value::Struct { properties, .. } => {
             let properties = if let Some(c_types) = c_types {
                 let mut filtered = Map::<Value>::new();
