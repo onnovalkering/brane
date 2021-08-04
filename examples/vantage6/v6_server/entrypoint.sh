db463d0934b23bc3821b2daa0bed253631808dae
Onno Valkering <onnovalkering@users.noreply.github.com>
2021-08-04 12:55:40 +0000

    chore(examples): run v6 import with older version inside a virtual env

--- examples/vantage6/v6_server/entrypoint.sh
+++ examples/vantage6/v6_server/entrypoint.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
-
 sed -i "s/PLC_SERVER_PORT/$SERVER_PORT/g" configuration.yml
 
+# Run import inside virtual environment with Vantage6 v1.2.3 installed.
+pipenv run vserver-local import --config ./configuration.yml --drop-all fixtures.yml
 vserver-local start --config "./configuration.yml"
 
 echo "~~>output: done"
