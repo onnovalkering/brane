f12a2b0ffd5722eae0ea6e1195740c7fe89a1768
Onno Valkering <onnovalkering@users.noreply.github.com>
2021-08-11 14:13:58 +0000

    fix(oas): correct parameter count tests

--- brane-oas/tests/build_input.rs
+++ brane-oas/tests/build_input.rs
@@ -15,10 +15,8 @@ fn param_count_matches_function_param_count() -> Result<()> {
 
     // For >=4 parameters, the parameters are grouped into a input type.
     let (function, types) = common::build_oas_function_param("/param-count/{1}/{2}/{3}/{4}", "onlyPathParameters")?;
-    assert_eq!(function.parameters.len(), 1);
-    assert_eq!(types.len(), 1);
-    let input_type = types.values().next().unwrap();
-    assert_eq!(input_type.properties.len(), 4);
+    assert_eq!(function.parameters.len(), 4);
+    assert_eq!(types.len(), 0);
 
     Ok(())
 }
@@ -26,10 +24,8 @@ fn param_count_matches_function_param_count() -> Result<()> {
 #[test]
 fn param_locations_are_irrelevant() -> Result<()> {
     let (function, types) = common::build_oas_function_param("/param-locations/{1}", "onlyPathParameters")?;
-    assert_eq!(function.parameters.len(), 1);
-    assert_eq!(types.len(), 1);
-    let input_type = types.values().next().unwrap();
-    assert_eq!(input_type.properties.len(), 4);
+    assert_eq!(function.parameters.len(), 4);
+    assert_eq!(types.len(), 0);
 
     Ok(())
 }
@@ -53,22 +49,8 @@ fn param_required_is_preserved() -> Result<()> {
 
     // For >=4 parameters, the parameters are grouped into a input type.
     let (function, types) = common::build_oas_function_param("/param-required-count-4", "onlyPathParameters")?;
-    assert_eq!(function.parameters.len(), 1);
-    assert_eq!(types.len(), 1);
-    let input_type = types.values().next().unwrap();
-    assert_eq!(input_type.properties.len(), 4);
-
-    assert!(input_type
-        .properties
-        .iter()
-        .filter(|p| p.name == String::from("1"))
-        .any(|p| p.optional.unwrap() == false));
-
-    assert!(input_type
-        .properties
-        .iter()
-        .filter(|p| p.name == String::from("2"))
-        .any(|p| p.optional.unwrap() == true));
+    assert_eq!(function.parameters.len(), 4);
+    assert_eq!(types.len(), 0);
 
     Ok(())
 }
@@ -100,10 +82,8 @@ fn body_object2props_2params() -> Result<()> {
 #[test]
 fn body_object4props_1param() -> Result<()> {
     let (function, types) = common::build_oas_function_body("/body-object-4", "onlyPathParameters")?;
-    assert_eq!(function.parameters.len(), 1);
-    assert_eq!(types.len(), 1);
-    let input_type = types.values().next().unwrap();
-    assert_eq!(input_type.properties.len(), 4);
+    assert_eq!(function.parameters.len(), 4);
+    assert_eq!(types.len(), 0);
 
     Ok(())
 }
