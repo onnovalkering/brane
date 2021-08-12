a4a9118e46092dfb7a61226e83ae010ac21eb33c
Onno Valkering <onnovalkering@users.noreply.github.com>
2021-08-12 10:54:39 +0000

    chore(examples): updated vantage6 packages

--- examples/vantage6/v6_node/entrypoint.sh
+++ examples/vantage6/v6_node/entrypoint.sh
@@ -2,8 +2,12 @@
 set -euo pipefail
 
 sed -i "s/PLC_API_KEY/$API_KEY/g" configuration.yml
+sed -i "s/PLC_DATA_PATH/${DATA_PATH//\//\\/}/g" configuration.yml
 sed -i "s/PLC_SERVER_HOST/$SERVER_HOST/g" configuration.yml
 sed -i "s/PLC_SERVER_PORT/$SERVER_PORT/g" configuration.yml
+
+# Generate random port number for the proxy.
+export PROXY_SERVER_PORT=$(shuf -i 2000-65000 -n 1)
 
 vnode-local start --config "./configuration.yml"
 
--- examples/vantage6/v6_server/entrypoint.sh
+++ examples/vantage6/v6_server/entrypoint.sh
@@ -5,6 +5,20 @@ sed -i "s/PLC_SERVER_PORT/$SERVER_PORT/g" configuration.yml
 
 # Run import inside virtual environment with Vantage6 v1.2.3 installed.
 pipenv run vserver-local import --config ./configuration.yml --drop-all fixtures.yml
+
+# Disable encryption for existing collaborations.
+pipenv run python -c " 
+from vantage6.cli.context import ServerContext;
+from vantage6.server.model.base import Database;
+
+ctx = ServerContext.from_external_config_file('./configuration.yml', 'prod', True);
+Database().connect(ctx.get_database_uri());
+
+import vantage6.server.db as db;
+for c in db.Collaboration.get(): c.encrypted = False; c.save();
+"
+
+# Start the server.
 vserver-local start --config "./configuration.yml"
 
 echo "~~>output: done"
